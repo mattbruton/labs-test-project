0cf95383231c706e2c0786276a29e270d87aa8ec
Matt Bruton <the.bruton@gmail.com>
2017-07-03 20:23:50 -0500

    minor changes to app.js in server side code, updated package json and lock file

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@
 require('dotenv').config();
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/api', routes);
+app.use('/', express.static(path.join(__dirname, '../dist')));
+
 
 module.exports = app;
